208484d3d08cc54e5c797833418af3b7db92aa30
n-shevko <nikdranik2@gmail.com>
2023-12-25 18:03:19 -0500

    perceive

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
 import torch
 import torch.nn as nn
 
+
 # https://distill.pub/2020/growing-ca/
 
-def perceive(state_grid):
-    sobel_x = torch.tensor(
-        [[-1, 0, +1],
-         [-2, 0, +2],
-         [-1, 0, +1]]
+
+def prepare_filters(in_channels):
+    sobel_x = [
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ]
+    sobel_y = [
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1]
+    ]
+    identity = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ]
+    filters = [sobel_x, sobel_y, identity]
+    # [out_channels, in_channels, kernel_height, kernel_width]
+    res = [[filter for _ in range(in_channels)] for filter in filters]
+    return torch.tensor(res, dtype=torch.float32)
+
+
+def perceive(state_grid, in_channels):
+    filters = prepare_filters(in_channels)
+    conv = nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=len(filters),
+        kernel_size=3,
+        bias=False,
+        stride=(3, 3),
     )
-    # out_channels = 1 assumed
-    sobel_y = sobel_x.transpose(0, 1)
-
-
-    in_channels = 3
-    out_channels = 64
-    kernel_size = 3
-    stride = 1
-    padding = 1
-
-    conv_layer = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-
-
-    input_data = torch.randn(1, 3, 32, 32)
-    output_data = conv_layer(input_data)
+    conv.weight.data = filters
+    return conv(state_grid)
 
 
 def main():
-    state_grid = torch.randn(10, 10, 16)
-    print(tensor_3d)
-    v = 3
+    in_channels = 16
+    # [batch_size, channels, height, width]
+    height = 3 * 4
+    width = 3 * 4
+    state_grid = torch.randn(1, in_channels, height, width)
+    print(state_grid.numpy())
+    print('*' * 100)
+    grads = perceive(state_grid, in_channels).detach().numpy()
+    print(grads.shape)
+    print(grads)
+
 
 main()
