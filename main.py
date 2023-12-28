0285a4fa8022cc721e706677db25b002bad453e5
n-shevko <nikdranik2@gmail.com>
2023-12-28 12:43:22 -0500

    filters

--- main.py
+++ main.py
@@ -6,35 +6,36 @@ import torch.nn as nn
 
 
 def prepare_filters(in_channels):
-    sobel_x = [
+    sobel_x = torch.tensor([
         [-1, 0, 1],
         [-2, 0, 2],
         [-1, 0, 1]
-    ]
-    sobel_y = [
+    ], dtype=torch.float32)
+    sobel_y = torch.tensor([
         [-1, -2, -1],
         [0, 0, 0],
         [1, 2, 1]
-    ]
-    identity = [
+    ], dtype=torch.float32)
+    identity = torch.tensor([
         [0, 0, 0],
         [0, 1, 0],
         [0, 0, 0]
-    ]
-    filters = [sobel_x, sobel_y, identity]
+    ], dtype=torch.float32)
+    filters = [sobel_x, sobel_y, identity] * in_channels
+    filters = torch.stack(filters).unsqueeze(1)
     # [out_channels, in_channels, kernel_height, kernel_width]
-    res = [[filter for _ in range(in_channels)] for filter in filters]
-    return torch.tensor(res, dtype=torch.float32)
+    return filters
 
 
 def perceive(state_grid, in_channels):
     filters = prepare_filters(in_channels)
     conv = nn.Conv2d(
         in_channels=in_channels,
-        out_channels=len(filters),
+        out_channels=3*in_channels,
         kernel_size=3,
         bias=False,
-        stride=(3, 3),
+        stride=1,
+        groups=in_channels
     )
     conv.weight.data = filters
     return conv(state_grid)
@@ -46,11 +47,8 @@ def main():
     height = 3 * 4
     width = 3 * 4
     state_grid = torch.randn(1, in_channels, height, width)
-    print(state_grid.numpy())
-    print('*' * 100)
     grads = perceive(state_grid, in_channels).detach().numpy()
     print(grads.shape)
-    print(grads)
 
 
 main()
